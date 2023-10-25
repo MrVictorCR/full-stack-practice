d3b2e7349cc620c120421c66cedb83e0e5736f10
Victor <vicmorh10@gmail.com>
2023-10-25 14:20:04 -0600

    JS Basics (All the JS Basics that I need)

---
+++ JS/hello_world.js
@@ -0,0 +1,133 @@
+
+/* 
+    Comments
+*/
+
+//- Hello World -//
+
+console.log("Hello World")
+
+// ------------ //
+
+//- Vars //
+
+// Let is going to be create a var but in this case var is going to be a GLOBAR VAR so, that is not "save at all"
+let myNumber = 6.8
+
+myNumber = 7 // Changing the value as normal
+
+let myBool = true // Bool var
+
+let myString = "Halo" // String
+
+const myConst = 7 // Constant, var that can not change
+
+let myNull = null // Null var
+
+// --- //
+
+
+// - Logical Operators - //
+
+/* 
+    and = &&
+    or = ||
+    not = !
+*/
+// ------------------- //
+
+// - Conditionals - //
+
+if (myNumber == 6){
+    console.log("The number is not " + 6)
+} else if (myNumber == 7){
+    console.log("The number is " + 7)
+} else {
+    console.log("The number is not " + 7)
+}
+
+// ---------------//
+
+// - Functions - //
+
+function myFunction(){
+    /* 
+        x instruction = the instructions that I want :)
+        return "hello world" = if we want to return something
+    */
+}
+
+// ---------------//
+
+// - List - //
+
+let myList = ["My Name", 4 , "Hello", "@lol"]
+console.log(myList[0]) // Get access to X part from the List
+
+// --------//
+
+// - Set - //
+
+let mySet = new Set(["My Name", 4 , "Hello", "@lol"])
+mySet.add("X") // Whatever that I want to add
+
+// ------ //
+
+// - Map - //
+
+let myMap = new Map([["My Name", 4], ["Victor", 21]]) // New Dict
+myMap.set("Dani", 32)
+console.log(myMap)
+console.log(myMap.get("Victor"))
+
+// ------ //
+
+// - Loops - //
+
+for (const value of myList){ // For
+    console.log(value)
+}
+
+let myCounter = 0
+while (myCounter <= myList){ // While with Counter
+    console.log(myList[myCounter])
+    myCounter++
+}
+
+// -------- //
+
+// - Class - //
+
+class MyClass {
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+}
+
+let myClass = new MyClass("Victo", 21)
+console.log(myClass)
+console.log(myClass.age)
+
+// -------- //
+
+// - Enum - //
+
+const MyEnum = {
+    DART: "dart",
+    PYTHON: "python",
+    SWIFT: "swift",
+    JAVA: "java",
+    KOTLIN: "kotlin",
+    JAVASCRIPT: "javascript"
+}
+
+const myEnum = MyEnum.JAVASCRIPT
+console.log(myEnum)
+
+// ------- //
+
+//-----------------------------------------------------------------------------------------------------------------//
+
+
+
