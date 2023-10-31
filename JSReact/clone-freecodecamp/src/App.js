87df865fb817acd6673883b21139655aab997631
Victor <vicmorh10@gmail.com>
2023-10-30 20:42:53 -0600

    Creating a Clone Practice (React Practice Number One)

---
+++ JSReact/clone-freecodecamp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Testimonio from './Components/testimonio';
+
+function App() {
+  return (
+    <div className="App">
+      <Testimonio />
+    </div>
+  );
+}
+
+export default App;
---
+++ JSReact/clone-freecodecamp/src/Components/testimonio.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+  function Testimonio(){
+    return(
+      <div className= 'contenedor-testimonio'> 
+
+        <img 
+          className='imagen-testimonio'
+          src={require('../img/testimonio-uno.jpg')}
+          alt='Pingüino Foto'
+        />
+        
+        <div className= 'contenedor-texto-testimonio'>
+          <p className= 'nombre-testimonio'>Pingüino de Madagascar</p>
+          <p className= 'cargo-testimonio'>Pingüino</p>
+          <p className= 'texto-testimonio'>"Soy un Pingüino de Madagascar el cual siempre soñó con salir a una aventura, lo cual me llevó al punto en el que me encuentro ahora que es viajando alrededor del mundo, todo esto ha sido posible gracias a que he aprendido a utilizar React!"</p>
+
+        </div>
+
+      </div>
+
+    )
+}
+  
+    export default Testimonio;
