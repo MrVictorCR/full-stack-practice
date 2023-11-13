0ebbfc0176e256f2d5c87bafe54f77bddac74057
Victor <vicmorh10@gmail.com>
2023-11-13 16:45:13 -0600

    NPX Create, first Commit

---
+++ JSReact/clicks-count/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import SVSLogo from './img/vsc-logo.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='logo-container'>
+        <img
+          className='logo'
+          src={SVSLogo} 
+          alt='Logo de SVS'/>
+      </div>
+      <div className='main-container'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
